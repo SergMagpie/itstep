e6ab6b2b1d67062983bfa99625f2fb652f7e2fc1
SergMagpie <sergmagpie@gmail.com>
2021-01-26 13:38:21 +0200

    Added homework from the third lesson

---
+++ lesson3/variables.py
@@ -0,0 +1,15 @@
+current_year = 2021
+light_speed_in_vacuum = 299792.458
+quote = '"Можливо все, неможливе просто потребує більше часу."\nДен Браун'
+oceans = ['Pacific', 'Athlantic', 'Indian', 'Southern', 'Arctic']
+active = False
+naming = tuple('itstep')
+school = {'students_amount': 5, 'students': ['Neo', 'Trinity', 'Morpheus', 
+          'Mouse', 'Tank'], 'subjects': ('Python', 'Math', 'English', 'Kung-Fu')}
+print(current_year)
+print(light_speed_in_vacuum)
+print(quote)
+print(oceans)
+print(active == False)
+print(naming)
+print(school)
