739bc0cf04ac162ffa0cb9a96d4b215552fa6248
SergMagpie <77747152+SergMagpie@users.noreply.github.com>
2021-02-08 21:22:07 +0200

    upgrade

---
+++ training_ground/all_words_matter.py
@@ -0,0 +1,7 @@
+text = '''Torches are made to light, jewels to wear,
+Dainties to taste, fresh beauty for the use,
+Herbs for their smell, and sappy plants to bear;
+Things growing to themselves are growth’s abuse,
+Seeds spring from seeds, and beauty breedeth beauty;
+Thou wast begot; to get it is thy duty.'''
+print(text.title())
