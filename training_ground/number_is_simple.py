940c28664d9010267cd2fbf1d48b82546db5bf22
SergMagpie <77747152+SergMagpie@users.noreply.github.com>
2021-02-15 21:16:14 +0200

    up

---
+++ training_ground/number_is_simple.py
@@ -0,0 +1,14 @@
+def is_num_simple(number):
+
+    if number == 2 or number == 3:
+        return True
+    elif number > 4:
+        for test in range(2, int(number ** 0.5 + 1)):
+            if number % test == 0:
+                return False
+            else:
+                return True
+
+
+for i in range(20):
+    print('number {} is simple - {}'.format(i, is_num_simple(i)))
