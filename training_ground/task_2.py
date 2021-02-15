e61bbd59e9823fdefe3e620d5751f53c1576a71e
SergMagpie <77747152+SergMagpie@users.noreply.github.com>
2021-02-15 21:42:19 +0200

    up

--- training_ground/task_2.py
+++ training_ground/task_2.py
@@ -1,5 +1,7 @@
 def odd_numbers(num1, num2):
     if num1 % 2:
-        return list(range(num1 + 1, num2))
+        return list(range(num1 + 1, num2, 2))
     else:
-        return
+        return list(range(num1, num2, 2))
+
+print(odd_numbers(3, 30))
