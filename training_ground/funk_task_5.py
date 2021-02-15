b92c09b1baa4d179f708b794b8d0b6a92227e276
SergMagpie <77747152+SergMagpie@users.noreply.github.com>
2021-02-15 20:53:30 +0200

    task 5

---
+++ training_ground/funk_task_5.py
@@ -0,0 +1,13 @@
+def sum_range(start_n, end_n):
+    if start_n < end_n:
+        n = start_n
+        sum_n = 0
+        while n <= end_n:
+            sum_n += n
+            n += 1
+        return sum_n
+    else:
+        return None
+
+
+print(sum_range(-5, 20))
