593cb2ce32b274aa4fc6ea890e58fc302c67cc0b
SergMagpie <77747152+SergMagpie@users.noreply.github.com>
2021-02-15 21:50:47 +0200

    up

---
+++ training_ground/task_3.py
@@ -0,0 +1,9 @@
+def drow_line(long, gorizont, char):
+    if gorizont:
+        s = ''
+    else:
+        s = '\n'
+    for _ in range(long):
+        print(char, end=s)
+
+drow_line(6, False, '8')
