594f06a9099614ec690ca249b7390926081499f6
SergMagpie <77747152+SergMagpie@users.noreply.github.com>
2021-02-17 21:47:15 +0200

    up

--- training_ground/global/main.py
+++ training_ground/global/main.py
@@ -1,10 +1,11 @@
-import coocoo, fib
-global pi
-pi = 3.14
 
-def main():
-    print(coocoo.coocoo(pi))
+import fib
 
-main()
+import time
 
-print(*[fib.fib(i) for i in range(1, 10)])
+start_time = time.time()
+print(*[fib.fib(i) for i in range(30)])
+print("-time recursion-- %s seconds ---" % (time.time() - start_time))
+start_time = time.time()
+print(*fib.fib_num(30))
+print("-time iteration-- %s seconds ---" % (time.time() - start_time))
