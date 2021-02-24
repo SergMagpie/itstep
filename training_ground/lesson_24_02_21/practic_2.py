3e5eb8debec9634b0afdbeb21403cef444028c45
SergMagpie <77747152+SergMagpie@users.noreply.github.com>
2021-02-24 22:57:55 +0200

    up

--- training_ground/lesson_24_02_21/practic_2.py
+++ training_ground/lesson_24_02_21/practic_2.py
@@ -1,57 +1,66 @@
-# task 1 -------------------------------------------------
-print('task 1 ----------------------------------------------\n')
+# # task 1 -------------------------------------------------
+# print('task 1 ----------------------------------------------\n')
 
 
-def gen_any_3():
-    for i in range(3, 230, 3):
-        yield i
+# def gen_any_3():
+#     for i in range(3, 230, 3):
+#         yield i
 
 
-gen = gen_any_3()
-print(next(gen))
-print(next(gen))
-print(next(gen))
-print(next(gen))
+# gen = gen_any_3()
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
 
-gen2 = iter(range(3, 230, 3))
-print(next(gen2))
-print(next(gen2))
-print(next(gen2))
-print(next(gen2))
+# gen2 = iter(range(3, 230, 3))
+# print(next(gen2))
+# print(next(gen2))
+# print(next(gen2))
+# print(next(gen2))
 
-# task 2--------------------------------------------------------------
-print('\ntask 2 -----------------------------------------------\n')
-
-
-def do_twice(funk_to_decorate):
-    def wrapper():
-        print('Funk for decor is decorated and it is runing twice')
-        funk_to_decorate()
-        funk_to_decorate()
-    return wrapper
+# # task 2--------------------------------------------------------------
+# print('\ntask 2 -----------------------------------------------\n')
 
 
-@do_twice
-def funk_for_dekor():
-    print("I'm funk for dekor")
+# def do_twice(funk_to_decorate):
+#     def wrapper():
+#         print('Funk for decor is decorated and it is runing twice')
+#         funk_to_decorate()
+#         funk_to_decorate()
+#     return wrapper
 
 
-funk_for_dekor()
-
-# task 3--------------------------------------------
-print('\ntask 3 -----------------------------\n')
-
-
-def repeat(funk):
-    def wrapper(times=1):
-        for _ in range(times):
-            funk(times)
-    return wrapper
+# @do_twice
+# def funk_for_dekor():
+#     print("I'm funk for dekor")
 
 
-@repeat
-def funk_for_repeat(n):
-    print(f'Funk for repeat {n} times')
+# funk_for_dekor()
+
+# # task 3--------------------------------------------
+# print('\ntask 3 -----------------------------\n')
 
 
-funk_for_repeat(4)
+def repeat(rep):
+    def _repeat(funk):
+        def wrapper():
+            for _ in range(rep):
+                funk()
+        return wrapper
+    return _repeat
+
+
+@repeat(3)
+def funk_for_repeat():
+    print('Funk for repeat times')
+
+
+funk_for_repeat()
+# ЗАРАБОТАЛО! нужна была двойная обертка, 
+# потому как тройка передается как аргумент 
+# а сама функция теряется, это уже особенности 
+# интерпретатора, для приема функции нужна вторая функция 
+# а аргсы и кваргсы нужны для передачи аргументов функции в функцию 
+# и вставляются уже в враппер, если я не прав, поправь, 
+# буду дальше изучать матчасть, спасибо за урок!
