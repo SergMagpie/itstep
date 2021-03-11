7f3821fee94d2bd0b5c9248a454da56f414cbdea
SergMagpie <77747152+SergMagpie@users.noreply.github.com>
2021-03-11 18:01:59 +0200

    Home work lesson 15

--- lesson15/add_new_spells.py
+++ lesson15/add_new_spells.py
@@ -1,6 +1,7 @@
-def add_new_spell(spells_list: set, new_spell: str) -> bool:
-    if new_spell not in spells_list:
-        spells_list.add(new_spell)
+def add_new_spells(character: set, *arcs) -> bool:
+    if not set(arcs) & character:
+        for i in arcs:
+            character.add(i)
         return True
     else:
         return False
@@ -14,11 +15,7 @@ if __name__ == "__main__":
              "Wingardium Leviosa",
              "Expelliarmus",
              "Expecto patronum"}
-    print(add_new_spell(Ron, "Expelliarmus"))
-    # True
-    print(Ron)
-    # {"Accio", "Wingardium Leviosa", "Alohomora", "Expelliarmus"}
-    print(add_new_spell(Harry, "Expelliarmus"))
+    print(add_new_spells(Harry, "Expelliarmus", "Lumos", "Obliviate"))
     # False
     print(Harry)
     # {"Accio", "Wingardium Leviosa", "Expelliarmus", "Expecto patronum"}
