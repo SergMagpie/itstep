7534a448660cf263b1def3c88a1783754fbe6dac
SergMagpie <77747152+SergMagpie@users.noreply.github.com>
2021-03-18 18:16:34 +0200

    home work lesson 18

--- lesson18/beautiful_list_generator.py
+++ lesson18/beautiful_list_generator.py
@@ -4,15 +4,26 @@ def beautiful_list_generator(lst: list, marker: str, file_name: str) -> bool:
     and place this list in the most acceptable
     form.
     '''
-
-    pass
+    try:
+        with open(file_name, 'w', encoding='utf-8') as f:
+            for n, i in enumerate(lst):
+                f.write(marker + i + ('\n' if n < len(lst) - 1 else ''))
+        return True
+    except:
+        return False
 
 
 if __name__ == "__main__":
     dc_heroes = [
         "Batman",
+        "Superman",
         "Flash",
         "Green Lantern",
         "Wonder Woman",
     ]
-    beautiful_list_generator(dc_heroes, "\u2713", "heroes.txt")
+    import os
+    # Actualised a directory with a script.
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+    os.chdir(dname)
+    print(beautiful_list_generator(dc_heroes, "\u2713", "heroes.txt"))
