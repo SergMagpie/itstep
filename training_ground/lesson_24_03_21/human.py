d6d573c04186d69cae29110ff76a31e76716396c
SergMagpie <77747152+SergMagpie@users.noreply.github.com>
2021-03-24 21:47:48 +0200

    up

--- training_ground/lesson_24_03_21/human.py
+++ training_ground/lesson_24_03_21/human.py
@@ -4,23 +4,30 @@ class Human():
         self.last_name = last_name
         self.date_of_birth = None
         self.phone_number = None
-        
+
     def set_date_of_birth(self, date_of_birth):
         self.date_of_birth = date_of_birth
 
     def set_phone_number(self, phone_number):
         self.phone_number = phone_number
-    
+
     def get_date_of_birth(self):
         return self.date_of_birth
 
     def get_phone_number(self):
         return self.phone_number
-    
+
     def get_first_name(self):
         return self.first_name
 
     def get_last_name(self):
         return self.last_name
 
-    
+    def greetings(self):
+        print("Hello, I'm {} {}!".format(self.last_name, self.first_name))
+
+    def contact_info(self):
+        print("My phone {}".format(self.phone_number))
+
+vasa = Human('Vasya', "Ivanovich")
+vasa.greetings()
