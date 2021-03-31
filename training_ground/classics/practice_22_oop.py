7597050a7d66bc7fd7c5905bb6873fd22f2e8764
SergMagpie <77747152+SergMagpie@users.noreply.github.com>
2021-03-31 21:20:50 +0300

    up

--- training_ground/classics/practice_22_oop.py
+++ training_ground/classics/practice_22_oop.py
@@ -1,19 +1,33 @@
 class CloneTrooper():
-    __counter = 0
-    def __init__(self):
-        self.__id = CloneTrooper.__counter
-        CloneTrooper.__counter += 1
+
+    def __init__(self, id):
+        self.__id = id
 
     def fight(self):
         print("I am CloneTrooper", self.__id, 'started fight')
 
-
     def say_name(self):
         print("I am CloneTrooper", self.__id)
 
+
+class StormTrooper(CloneTrooper):
+    def __init__(self, id):
+        super().__init__(id)
+        self.__id = "ST-" + id
+
+    def type(self):
+        print('Trooper type -> StormTrooper', self.__id)
+
+
 if __name__ == "__main__":
     stek = []
+    stek2 = []
     for i in range(10):
-        stek.append(CloneTrooper())
+        stek.append(CloneTrooper(str(i)))
     [i.fight() for i in stek]
     [i.say_name() for i in stek]
+    for i in range(10):
+        stek2.append(StormTrooper(str(i)))
+    [i.fight() for i in stek2]
+    [i.say_name() for i in stek2]
+    [i.type() for i in stek2]
