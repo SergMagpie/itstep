50d558a88342b3025af2bcd3e25a672d690c5aa0
SergMagpie <77747152+SergMagpie@users.noreply.github.com>
2021-04-05 21:37:24 +0300

    up

--- training_ground/hlam/123.py
+++ training_ground/hlam/123.py
@@ -1,31 +1,24 @@
-import random
-KeyList = ['1:00', '2:00', '3:00', '4:00', '5:00',
-           '6:00', '7:00', '8:00', '9:00', '10:00',
-           '11:00', '12:00', '13:00', '14:00', '15:00',
-           '16:00', '17:00', '18:00', '19:00', '20:00',
-           '21:00', '22:00', '23:00', '24:00']
-# словарь для записи в значение играл/не играл(0 - не играл, 1 - играл)
-time = {}
-for i in KeyList:
-    time[i] = random.randint(0, 1)
+class MyClass:
+    pass
 
-print(time)
 
-# перенос значений в список, чтобы сравнить два списка
-values = []
-for v in time.values():
-    values.append(v)
-print(KeyList)
-print(values)
+obj = MyClass()
+obj.a = 1
+obj.b = 2
+obj.i = 3
+obj.ireal = 3.5
+obj.integer = 4
+obj.z = 5
 
-# собственно, решение задачи
-count = 0
-max_item = 0
-for i in time.values():
-    if i:
-        count += 1
-    else:
-        count = 0
-    if count > max_item:
-        max_item = count
-print(max_item)
+
+def incIntsI(obj):
+    for name in obj.__dict__.keys():
+        if name.startswith('i'):
+            val = getattr(obj, name)
+            if isinstance(val, int):
+                setattr(obj, name, val + 1)
+
+
+print(obj.__dict__)
+incIntsI(obj)
+print(obj.__dict__)
