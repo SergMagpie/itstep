c6c4ed9e780c8b448c85d16eed1c6288fb12d6dc
SergMagpie <77747152+SergMagpie@users.noreply.github.com>
2021-04-05 22:07:06 +0300

    up

--- training_ground/practic/war_craft.py
+++ training_ground/practic/war_craft.py
@@ -1,3 +1,7 @@
+class Army:
+    pass
+
+
 class OrcArmy:
     def __init__(self, warior_amount, damage_per_orc, warrior_health) -> None:
         self.warior_amount = warior_amount
@@ -29,8 +33,12 @@ class OrcArmy:
         return OrcArmy(new_amount, new_damage, new_health)
 
     def __str__(self) -> str:
+        if self.warior_amount > 0:
+            warior_amount = self.warior_amount
+        else:
+            warior_amount = 'Army is destroyed'
         return ' '.join([str(i) for i in
-                         ['Orc ', self.warior_amount, self.damage_per_orc,
+                         ['Orc ', warior_amount, self.damage_per_orc,
                           self.warrior_health]])
 
     def receive_damage(self, damage: int):
@@ -75,8 +83,12 @@ class ElfArmy:
         return ElfArmy(new_amount, new_damage, new_health, new_shield)
 
     def __str__(self) -> str:
+        if self.warior_amount > 0:
+            warior_amount = self.warior_amount
+        else:
+            warior_amount = 'Army is destroyed'
         return ' '.join([str(i) for i in
-                         ['Elf ', self.warior_amount, self.damage_per_orc,
+                         ['Elf ', warior_amount, self.damage_per_orc,
                           self.warrior_health, self.shield]])
 
     def receive_damage(self, damage: int):
@@ -105,3 +117,16 @@ if __name__ == "__main__":
     print(b4)
     b4.receive_damage(500)
     print(b4)
+    steck = [a1, a2, a3, a4, b1, b2, b3, b4]
+    damage = 0
+    while damage != 'exit':
+        damage = input('Enter damage or exit for exit ')
+        if damage.isdigit():
+            damage = int(damage)
+            for i in steck:
+                i.receive_damage(damage)
+            print(*steck, sep="\n")
+        elif damage == 'exit':
+            print('Good by!')
+        else:
+            print('You made a mistake')
