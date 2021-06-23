18f5eb93550106541552d59ea459497d3d4d086f
SergMagpie <77747152+SergMagpie@users.noreply.github.com>
2021-06-23 21:08:40 +0300

    up

--- training_ground/practic/library/books/urls.py
+++ training_ground/practic/library/books/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home'),
+    path('', BooksListView.as_view(), name='home'),
 ]
--- training_ground/practic/library/books/views.py
+++ training_ground/practic/library/books/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import *
 
 # Create your views here.
 def index(request):
     return HttpResponse(f'contact')
+
+class BooksListView(ListView):
+    model = Books
+    paginate_by = 2
+
+    
