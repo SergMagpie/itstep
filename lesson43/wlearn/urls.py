4736c48ae3cfbad26a491532514e16cf58e9ac25
SergMagpie <77747152+SergMagpie@users.noreply.github.com>
2021-06-25 13:38:03 +0300

    up

--- lesson43/wlearn/urls.py
+++ lesson43/wlearn/urls.py
@@ -4,8 +4,8 @@ from wlearn.views import *
 urlpatterns = [
     path("", index, name='home'),
     path("words", words, name='words'),
-    path("new-user", new_user, name='new_user'),
-    path("login", login, name='login'),
+    path("new-user", RegisterUser.as_view(), name='new_user'),
+    path("login", LoginUser.as_view(), name='login'),
     path("add-word", add_word, name='add_word'),
     path("first-task", first_task, name='first_task'),
 ]
--- lesson43/wlearn/views.py
+++ lesson43/wlearn/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
 
 # Create your views here.
 main_menu = [
@@ -13,27 +17,72 @@ main_menu = [
 
 
 def index(request):
-    string = ''
-    for i in main_menu:
-        string += f"<a href='{i['url_name']}'>{i['title']}</a><br>"
-    return HttpResponse(f"<h1>The index page</h1>{string}")
+    context = {
+        'title': 'lesson 43',
+     }
+    return render(request, 'wlearn/home.html', context=context)
 
-def new_user(request):
-    return HttpResponse("The new_user page")
+# def new_user(request):
+#     return HttpResponse("The new_user page")
+
 
 def words(request):
     return HttpResponse("The words page")
 
-def login(request):
-    return HttpResponse("The login page")
+# def login(request):
+#     return HttpResponse("The login page")
+
 
 def add_word(request):
     return HttpResponse("The add_word page")
 
+
 def first_task(request):
-    return HttpResponse("The first_task page")
+    rez, numbers, mission = None, [], None
+    if request.method == 'POST':
+        numbers = [
+            int(request.POST.get('first_number') or 0),
+            int(request.POST.get('second_number') or 0),
+            int(request.POST.get('third_number') or 0),
+        ]
+        mission = request.POST.get('mission')
+        missions = {
+            'minimum': min,
+            'maximum': max,
+            'mean': lambda x: sum(x) / len(x)
+        }
+        if mission in missions:
+            rez = missions[mission](numbers)
+    context = {
+        'title': 'First task with lesson 43',
+        'rez': rez,
+        'numbers': numbers,
+        'mission': mission,
+    }
+    return render(request, 'wlearn/first_task.html', context=context)
+
 
 class RegisterUser(CreateView):
     form_class = UserCreationForm
     template_name = 'wlearn/register.html'
-    success_url = reverse_lasy('login')
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # self.get_user_context(title='Registration')
+        c_def = {'title': 'Registration'}
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+class LoginUser(LoginView):
+    form_class = AuthenticationForm
+    template_name = 'wlearn/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # self.get_user_context(title='Registration')
+        c_def = {'title': 'Login'}
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_success_url(self) -> str:
+        return reverse_lazy('words')
